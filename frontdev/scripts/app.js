4036c92a055f7d5810d9e0be1901378846b4ae4a
Roxana Macarie <roxana_macarie2000@yahoo.com>
2016-06-23 12:47:50 -0700

    Day 1 Frontend workshop

---
+++ frontdev/scripts/app.js
@@ -0,0 +1,83 @@
+var employeesList = [
+    {
+        firstName: 'John',
+        lastName: 'King',
+        phone: '0123456789',
+        salary: 4500
+    },
+    {
+        firstName: 'Steven',
+        lastName: 'Gerard',
+        phone: '0123456789',
+        salary: 4500
+    },
+    {
+        firstName: 'Diana',
+        lastName: 'Ross',
+        phone: '0123456789',
+        salary: 4500
+    },
+    {
+        firstName: 'Mike',
+        lastName: 'Bob',
+        phone: '0123456789',
+        salary: 4500
+    },
+    {
+        firstName: 'Emily',
+        lastName: 'Hudson',
+        phone: '0123456789',
+        salary: 4500
+    }
+];
+
+function showList() {
+    var myTable = '<table class="table table-striped" border="1"><tr><th> First Name </th><th> Last Name</th><th>Phone </th><th> Salary </th></tr>';
+
+    for (var i in employeesList) {
+        myTable +=
+            '<tr><td>' + employeesList[i].firstName + '</td><td>' + employeesList[i].lastName + '</td><td>' + employeesList[i].phone + '</td><td>' + employeesList[i].salary + '</td></tr>';
+
+    }
+
+    myTable += '</table>';
+    var container = document.getElementById('listcontainer');
+    container.innerHTML = myTable;
+}
+
+var Employee = function (firstName, lastName, phone, salary){
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.phone=phone;
+    this.salary=salary;
+}
+
+function newEmployee(){
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var phone = document.getElementById("phone").value;
+    var salary = document.getElementById("salary").value;
+    employeesList.push(new Employee(firstName,lastName,phone,salary));
+}
+
+function addEmployee(){
+    newEmployee();
+    showList();
+}
+
+function showSalary(){
+    
+    var sum=0;
+    for(var i in employeesList){
+        sum += parseFloat(employeesList[i].salary);
+    }
+    
+    var container = document.getElementById('salarycontainer');
+    container.textContent= sum;
+}
+
+function deleteLastEmployee(){
+    employeesList.pop();
+
+    showList();
+}
