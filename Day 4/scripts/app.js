4d4b0eac4d3384ec8516b252a8c5092805d457f0
Roxana Macarie <roxana_macarie2000@yahoo.com>
2016-06-28 11:19:44 -0700

    Day 4

---
+++ Day 4/scripts/app.js
@@ -0,0 +1,21 @@
+var hrApp = angular.module('hrApp', ['ngRoute']);
+
+
+hrApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/colors', {
+            templateUrl: 'views/colors.html',
+            controller: 'ColorsController'
+        })
+        .when('/forms', {
+            templateUrl:'views/form.html',
+            controller: 'FormsController'
+        })
+
+    /*.otherwise({
+       redirectTo: 'views/main.html'
+    });*/
+}]);
+
+
+
+
